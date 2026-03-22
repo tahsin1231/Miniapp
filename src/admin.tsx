c5d2305000735a59da0800af3feff881f0759c9b
tahsin1231 <binanceverify118@gmail.com>
2026-03-22 13:25:43 +0600

    Add files via upload

---
+++ src/admin.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AdminPanel from './AdminPanel';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AdminPanel />
+  </React.StrictMode>
+);
